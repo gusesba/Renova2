2e1aaa375f8717c154d1cb06f47e34a5913973ea
Gustavo Esmanhotto Bareta <gustavo.bareta@mps.interno>
2026-03-16 10:26:17 -0300

    Solução base

---
+++ Backend/Persistencia/RenovaDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Renova.Persistence
+{
+    public class RenovaDbContext : DbContext
+    {
+        public RenovaDbContext(DbContextOptions<RenovaDbContext> options) : base(options) { }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
